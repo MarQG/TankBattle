a069c5fca300f788b2238b44890d3bca525a42c2
MarQG <ferenc.g87@gmail.com>
2018-05-31 14:00:11 -0500

    Added PlayerFinding in Tank AI Controller

--- TankBattle/Source/TankBattle/Private/TankAIController.cpp
+++ TankBattle/Source/TankBattle/Private/TankAIController.cpp
@@ -1,6 +1,6 @@
 // 2018 Copyright Rule One Computer Services
 
-
+#include "Engine/World.h"
 #include "../Public/TankAIController.h"
 
 ATank * ATankAIController::GetControlledTank() const
@@ -8,19 +8,30 @@ ATank * ATankAIController::GetControlledTank() const
 	return Cast<ATank>(GetPawn());
 }
 
+ATank * ATankAIController::GetPlayerTank() const
+{
+	auto PlayerControlledPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerControlledPawn)
+	{
+		return nullptr;
+	}
+	return Cast<ATank>(PlayerControlledPawn);
+}
+
 void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	auto AIControlledTank = GetControlledTank();
-	if (!AIControlledTank) 
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank) 
 	{
-		UE_LOG(LogTemp, Warning, TEXT("AIController not possessing a tank"));
-		return;
+		UE_LOG(LogTemp, Warning, TEXT("AIControllercan find player tank"));
 	}
 	else 
 	{
-		FString CurrentOwner = AIControlledTank->GetName();
-		UE_LOG(LogTemp, Warning, TEXT("AIController possessing: %s"), *CurrentOwner);
+		FString CurrentPlayerTank = PlayerTank->GetName();
+		UE_LOG(LogTemp, Warning, TEXT("AIController found player tank: %s"), *CurrentPlayerTank);
 	}
+
+	
 }
--- TankBattle/Source/TankBattle/Public/TankAIController.h
+++ TankBattle/Source/TankBattle/Public/TankAIController.h
@@ -15,7 +15,10 @@ class TANKBATTLE_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
 
+private:
 	ATank* GetControlledTank() const;
+
+	ATank* GetPlayerTank() const;
 	
 	virtual void BeginPlay() override;
 };
